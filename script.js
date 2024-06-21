4d87ae4fcc36c1e9cd57ef0b2137012ff7d32f4c
Rupesh Saha <rupeshsaha899@gmail.com>
2024-06-21 12:25:59 +0530

    updates

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
 let createBtn = document.querySelector(".create-new");
 let taskContainer = document.querySelector(".task-container");
 let inputTask = document.querySelector(".input-task");
+let delBtn
 
 createBtn.addEventListener("click", () => {
     if (inputTask.value.trim() === "") {
         alert("Task cannot be empty");
-    } else {
+    } 
+    else
+     {
         let newTask = document.createElement("div");
         newTask.classList.add("todos");
 
         let newTitle = document.createElement("p");
         newTitle.innerText = inputTask.value.trim();
+        newTitle.style.fontSize = "18px"
         newTask.appendChild(newTitle);
+
+        delBtn = document.createElement("i");
+        delBtn.classList.add("fa-solid");   
+        delBtn.classList.add("fa-trash");   
+        delBtn.style.color = "#eb1818"
+        delBtn.style.position = "absolute";
+        delBtn.style.right = "10px";
+        delBtn.style.margin = "2px"
+
+        delBtn.addEventListener("click",()=>{
+            delBtn.parentElement.remove();
+            saveData()
+        })
+
+     
+        
+        
+       
+        newTask.addEventListener("click", ()=>{
+            newTitle.classList.toggle("checked");
+            saveData()
+        })
+        
+       
+
+        newTask.appendChild(delBtn);
+      
 
         taskContainer.appendChild(newTask);
         saveData(); // Save data after adding a new task
@@ -24,13 +55,16 @@ function saveData() {
     localStorage.setItem("tasks", taskContainer.innerHTML);
 }
 
-// Load tasks from localStorage and display
+// // Load tasks from localStorage and display
+
+
 function loadData() {
     let savedTasks = localStorage.getItem("tasks");
     if (savedTasks) {
         taskContainer.innerHTML = savedTasks;
     }
+
 }
 
-// Call loadData when the page loads to restore saved tasks
+// // Call loadData when the page loads to restore saved tasks
 document.addEventListener("DOMContentLoaded", loadData);
